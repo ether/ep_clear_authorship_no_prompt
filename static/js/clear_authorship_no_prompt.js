a0dd068b11140441d6740d3a7d7317aa75dcaeb0
John McLear <john@mclear.co.uk>
2014-11-07 00:55:30 +0000

    fix it as it got b0rked

--- static/js/clear_authorship_no_prompt.js
+++ static/js/clear_authorship_no_prompt.js
@@ -1,7 +1,7 @@
 var clear_authorship_no_prompt = {
   init: function(context) {
-    $('#clearAuthorship').off('click'); // remove pre-existing events
-    $('#clearAuthorship').click(function(){ // add new click event handler
+    $('.buttonicon-clearauthorship').parent().parent().off('click'); // remove pre-existing events
+    $('.buttonicon-clearauthorship').parent().parent().click(function(){ // add new click event handler
       context.ace.callWithAce(function(ace){ // call the function to apply the attribute inside ACE
         if ((!(ace.ace_getRep().selStart && ace.ace_getRep().selEnd)) || ace.ace_isCaret()){
           ace.ace_performDocumentApplyAttributesToCharRange(0, ace.ace_getRep().alltext.length, [['author', '']]);
