f5a4fd7d51918e24683912c1054cc661c1544711
John McLear <john@mclear.co.uk>
2021-02-04 11:59:11 +0000

    lint

--- index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const eejs = require('ep_etherpad-lite/node/eejs');
-
-exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content = `<script src="../static/plugins/ep_clear_authorship_no_prompt/static/js/clear_authorship_no_prompt.js"></script>${args.content}`;
-  return cb();
-};
--- static/js/init.js
+++ static/js/init.js
@@ -1,3 +1,15 @@
-exports.postAceInit = function (hook, context) {
-  clear_authorship_no_prompt.init(context);
+'use strict';
+
+exports.postAceInit = (hook, context) => {
+  $('.buttonicon-clearauthorship').parent().parent().off('click'); // remove pre-existing events
+  $('.buttonicon-clearauthorship').parent().parent().click(() => { // add new click event handler
+    context.ace.callWithAce((ace) => { // call the function to apply the attribute inside ACE
+      if ((!(ace.ace_getRep().selStart && ace.ace_getRep().selEnd)) || ace.ace_isCaret()) {
+        ace.ace_performDocumentApplyAttributesToCharRange(
+            0, ace.ace_getRep().alltext.length, [['author', '']]);
+      } else {
+        ace.ace_setAttributeOnSelection('author', '');
+      }
+    }, 'clearAuthorship', true);
+  });
 };
